d71429eb7200549f23ea53e517269af4a2a42356
Rupeshrj36 <155728586+Rupeshrj36@users.noreply.github.com>
2026-06-09 15:10:23 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        vector<int> count_s(26, 0);
+        vector<int> count_t(26, 0);
+
+        for(int i=0; i<s.length(); i++){
+            count_s[s[i] - 'a']++;
+        }
+
+        for(int i=0; i<t.length(); i++){
+            count_t[t[i] - 'a']++;
+        }
+
+        for(int i=0; i<26; i++){
+            if(count_s[i] != count_t[i]) return false;
+        }
+
+    return true;
+    }
+};
